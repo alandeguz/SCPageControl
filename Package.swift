057d7a836a481ba13d0094fb1befb488038cd1aa
Alan DeGuzman <alan.a.deguzman@ehi.com>
2022-10-02 22:17:58 -0500

    Swift Package Manager support

---
+++ Package.swift
@@ -0,0 +1,12 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(name: "SCPageControl",
+                      platforms: [.iOS(.v12)],
+                      products: [.library(name: "SCPageControl",
+                                          targets: ["SCPageControl"])],
+                      targets: [.target(name: "SCPageControl",
+                                        path: "SCPageControl/Classes")],
+                      swiftLanguageVersions: [.v5])
